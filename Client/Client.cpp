31f72b5c4fc93cf75a17c3aef0d16f9480169257
wny0320 <wny0320@naver.com>
2025-07-01 15:13:46 +0900

    로그인/아웃 구현
    로그인/아웃 구현 및 클라이언트(싱글)의 위치 저장 구현

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -5,18 +5,131 @@
 
 #include <iostream>
 #include <WinSock2.h>
+#include <Windows.h>
 #include <conio.h>
-#include "Position_generated.h"
-#include "InputData_generated.h"
+
+#include "flatbuffers/flatbuffers.h"
+#include "UserEvents_generated.h"
+
 
 #pragma comment(lib, "ws2_32")
+
+uint32_t MyPlayerId = 0;
+
+uint64_t GetTimeStamp()
+{
+	return (uint64_t)time(NULL);
+}
+
+void SendPacket(SOCKET Socket, flatbuffers::FlatBufferBuilder& Builder)
+{
+	int PacketSize = (int)Builder.GetSize();
+	PacketSize = ::htonl(PacketSize);
+	//header, 길이
+	int SentBytes = ::send(Socket, (char*)&PacketSize, sizeof(PacketSize), 0);
+	//자료 
+	SentBytes = ::send(Socket, (char*)Builder.GetBufferPointer(), Builder.GetSize(), 0);
+	if (SentBytes <= 0)
+	{
+		std::cout << "Send failed: " << WSAGetLastError() << std::endl;
+	}
+}
+
+void RecvPacket(SOCKET Socket, char* Buffer)
+{
+	int PacketSize = 0;
+	int RecvBytes = recv(Socket, (char*)&PacketSize, sizeof(PacketSize), MSG_WAITALL);
+	if (RecvBytes <= 0)
+	{
+		std::cout << "Header Recv failed: " << WSAGetLastError() << std::endl;
+		return;
+	}
+	PacketSize = ntohl(PacketSize);
+	RecvBytes = recv(Socket, Buffer, PacketSize, MSG_WAITALL);
+	if (RecvBytes <= 0)
+	{
+		std::cout << "Body Recv failed: " << WSAGetLastError() << std::endl;
+		return;
+	}
+}
+void CreateC2S_Login(flatbuffers::FlatBufferBuilder& Builder)
+{
+	if (MyPlayerId > 0)
+	{
+		return;
+	}
+	auto LoginEvent = UserEvents::CreateC2S_Login(Builder, Builder.CreateString("username"), Builder.CreateString("password"));
+	auto EventData = UserEvents::CreateEventData(Builder, GetTimeStamp(), UserEvents::EventType_C2S_Login, LoginEvent.Union());
+	Builder.Finish(EventData);
+}
+void CreateC2S_Logout(flatbuffers::FlatBufferBuilder& Builder)
+{
+	if (MyPlayerId <= 0)
+	{
+		return;
+	}
+	auto LogoutEvent = UserEvents::CreateC2S_Logout(Builder, MyPlayerId);
+	auto EventData = UserEvents::CreateEventData(Builder, GetTimeStamp(), UserEvents::EventType_C2S_Logout, LogoutEvent.Union());
+	Builder.Finish(EventData);
+}
+void CreateC2S_PlayerMoveData(flatbuffers::FlatBufferBuilder& Builder, uint8_t KeyCode)
+{
+	if (MyPlayerId <= 0)
+	{
+		return;
+	}
+	auto PlayerMoveEvent = UserEvents::CreateC2S_PlayerMoveData(Builder, MyPlayerId, KeyCode);
+	auto EventData = UserEvents::CreateEventData(Builder, GetTimeStamp(), UserEvents::EventType_C2S_PlayerMoveData, PlayerMoveEvent.Union());
+	Builder.Finish(EventData);
+}
+void ProcessPacket(const char* RecvBuffer)
+{
+	//root_type
+	auto RecvEventData = UserEvents::GetEventData(RecvBuffer);
+	//std::cout << RecvEventData->timestamp() << std::endl; //타임스탬프
+
+	switch (RecvEventData->data_type())
+	{
+	case UserEvents::EventType_S2C_Login:
+	{
+		auto LoginData = RecvEventData->data_as_S2C_Login();
+		MyPlayerId = LoginData->player_id();
+		system("cls");
+		COORD Cur;
+		Cur.X = LoginData->position_x();
+		Cur.Y = LoginData->position_y();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+		std::cout << "*";
+	}
+	break;
+	case UserEvents::EventType_S2C_Logout:
+	{
+		auto LogoutData = RecvEventData->data_as_S2C_Logout();
+		MyPlayerId = 0;
+		system("cls");
+	}
+	break;
+	case UserEvents::EventType_S2C_PlayerMoveData:
+	{
+		auto MoveData = RecvEventData->data_as_S2C_PlayerMoveData();
+		system("cls");
+		COORD Cur;
+		Cur.X = MoveData->position_x();
+		Cur.Y = MoveData->position_y();
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+		std::cout << "*";
+	}
+	break;
+	}
+}
+
 
 int main()
 {
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET ServerSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKET ServerSocket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	SOCKADDR_IN ServerSockAddr;
 	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
@@ -24,49 +137,52 @@ int main()
 	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	ServerSockAddr.sin_port = htons(30303);
 
-	connect(ServerSock, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
-
-	int X = 0;
-	int Y = 0;
-	std::cout << "*";
+	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
 	while (true)
 	{
 		char Input;
 		if (_kbhit())
 		{
-			system("cls");
 			Input = _getch();
 		}
 		else
 		{
 			continue;
 		}
-		flatbuffers::FlatBufferBuilder Builder(1024);
-		auto InputData = Move::CreateInputData(Builder, Input);
-		Builder.Finish(InputData);
+		char RecvBuffer[10240] = { 0 };
+		flatbuffers::FlatBufferBuilder Builder;
 
-		int PacketSize = (int)Builder.GetSize();
-		PacketSize = htonl(PacketSize);
-		int SentBytes = send(ServerSock, (char*)&PacketSize, sizeof(PacketSize), 0);
-		SentBytes = send(ServerSock, (char*)Builder.GetBufferPointer(), (int)Builder.GetSize(), 0);
+		switch (Input)
+		{
+		case 'w':
+		case 's':
+		case 'a':
+		case 'd':
+			CreateC2S_PlayerMoveData(Builder, Input);
+			break;
+		case 'i':
+			CreateC2S_Login(Builder);
+			break;
+		case 'o':
+			CreateC2S_Logout(Builder);
+			break;
+		default:
+			break;
+		}
 
-		char RecvBuffer[65535] = { 0, };
-		int RecvBytes = recv(ServerSock, (char*)&PacketSize, sizeof(PacketSize), MSG_WAITALL);
-		PacketSize = ntohl(PacketSize);
-		RecvBytes = recv(ServerSock, RecvBuffer, PacketSize, MSG_WAITALL);
+		if (Builder.GetSize() <= 0)
+		{
+			continue;
+		}
 
-		auto Data = Move::GetPositionData(RecvBuffer);
+		SendPacket(ServerSocket, Builder);
 
-		X = Data->x();
-		Y = Data->y();
-		COORD Cur;
-		Cur.X = X;
-		Cur.Y = Y;
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-		std::cout << "*";
+		RecvPacket(ServerSocket, RecvBuffer);
+
+		ProcessPacket(RecvBuffer);
 	}
 
-	closesocket(ServerSock);
+	closesocket(ServerSocket);
 
 	WSACleanup();
 
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,91 +1,195 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #define NOMINMAX
-#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING
 
 #include <iostream>
 #include <WinSock2.h>
-#include <algorithm>
-#include "Position_generated.h"
-#include "InputData_generated.h"
+
+#include "flatbuffers/flatbuffers.h"
+#include "UserEvents_generated.h"
+
 
 #pragma comment(lib, "ws2_32")
+
+uint16_t PosX = 0;
+uint16_t PosY = 0;
+
+uint64_t GetTimeStamp()
+{
+	return (uint64_t)time(NULL);
+}
+
+int ProcessPacket(const char* RecvBuffer, SOCKET ClientSocket);
+
+
+void SendPacket(SOCKET Socket, flatbuffers::FlatBufferBuilder& Builder)
+{
+	int PacketSize = (int)Builder.GetSize();
+	PacketSize = ::htonl(PacketSize);
+	//header, 길이
+	int SentBytes = ::send(Socket, (char*)&PacketSize, sizeof(PacketSize), 0);
+	//자료 
+	SentBytes = ::send(Socket, (char*)Builder.GetBufferPointer(), Builder.GetSize(), 0);
+	if (SentBytes <= 0)
+	{
+		std::cout << "Send failed: " << WSAGetLastError() << std::endl;
+	}
+}
+
+void RecvPacket(SOCKET Socket, char* Buffer)
+{
+	int PacketSize = 0;
+	int RecvBytes = recv(Socket, (char*)&PacketSize, sizeof(PacketSize), MSG_WAITALL);
+	if (RecvBytes <= 0)
+	{
+		std::cout << "Header Recv failed: " << WSAGetLastError() << std::endl;
+		return;
+	}
+	PacketSize = ntohl(PacketSize);
+	RecvBytes = recv(Socket, Buffer, PacketSize, MSG_WAITALL);
+	if (RecvBytes <= 0)
+	{
+		std::cout << "Body Recv failed: " << WSAGetLastError() << std::endl;
+		return;
+	}
+}
+
+
+void CreateS2C_Login(flatbuffers::FlatBufferBuilder& Builder, bool IsSuccess, std::string Message, uint16_t PosX, uint16_t PosY, UserEvents::Color Color, uint32_t PlayerId)
+{
+	auto LoginEvent = UserEvents::CreateS2C_Login(Builder, IsSuccess, Builder.CreateString(Message), PosX, PosY, &Color, PlayerId);
+	auto EventData = UserEvents::CreateEventData(Builder, GetTimeStamp(), UserEvents::EventType_S2C_Login, LoginEvent.Union());
+	Builder.Finish(EventData);
+}
+
+void CreateS2C_Logout(flatbuffers::FlatBufferBuilder& Builder, uint32_t PlayerId, bool IsSuccess, std::string Message)
+{
+	auto LogoutEvent = UserEvents::CreateS2C_Logout(Builder, PlayerId, IsSuccess, Builder.CreateString(Message));
+	auto EventData = UserEvents::CreateEventData(Builder, GetTimeStamp(), UserEvents::EventType_S2C_Logout, LogoutEvent.Union());
+	Builder.Finish(EventData);
+}
+
+void CreateS2C_PlayerMoveData(flatbuffers::FlatBufferBuilder& Builder, uint32_t PlayerId, uint16_t PosX, uint16_t PosY)
+{
+	auto PlayerMoveEvent = UserEvents::CreateS2C_PlayerMoveData(Builder, PlayerId, PosX, PosY);
+	auto EventData = UserEvents::CreateEventData(Builder, GetTimeStamp(), UserEvents::EventType_S2C_PlayerMoveData, PlayerMoveEvent.Union());
+	Builder.Finish(EventData);
+}
 
 int main()
 {
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET ListenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	sockaddr_in ListenSockAddr;
+	SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN ListenSockAddr;
 	memset(&ListenSockAddr, 0, sizeof(ListenSockAddr));
-	ListenSockAddr.sin_family = AF_INET;
+	ListenSockAddr.sin_family = PF_INET;
 	ListenSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	ListenSockAddr.sin_port = htons(30303);
 
-	bind(ListenSock, (sockaddr*)&ListenSockAddr, sizeof(ListenSockAddr));
+	bind(ListenSocket, (SOCKADDR*)&ListenSockAddr, sizeof(ListenSockAddr));
 
-	listen(ListenSock, 5);
-
-	sockaddr_in ClientSockAddr;
+	listen(ListenSocket, 5);
+	SOCKADDR_IN ClientSockAddr;
 	memset(&ClientSockAddr, 0, sizeof(ClientSockAddr));
 	int ClientSockAddrLength = sizeof(ClientSockAddr);
-	SOCKET ClientSock = accept(ListenSock, (sockaddr*)&ClientSockAddr, &ClientSockAddrLength);
+	SOCKET ClientSocket = accept(ListenSocket, (SOCKADDR*)&ClientSockAddr, &ClientSockAddrLength);
 
-	int X = 0;
-	int Y = 0;
-	std::cout << "*";
 	while (true)
 	{
-		int PacketSize = 0;
 		char RecvBuffer[4000] = { 0, };
-		int RecvBytes = recv(ClientSock, (char*)&PacketSize, sizeof(PacketSize), MSG_WAITALL);
-		PacketSize = ntohl(PacketSize);
-		if (RecvBytes <= 0)
-		{
-			continue;
-		}
-		system("cls");
-		RecvBytes = recv(ClientSock, RecvBuffer, PacketSize, MSG_WAITALL);
+		RecvPacket(ClientSocket, RecvBuffer);
 
-		auto InputData = Move::GetInputData(RecvBuffer);
-
-		switch (InputData->input())
-		{
-		case 'w':
-			Y--;
-			break;
-		case 's':
-			Y++;
-			break;
-		case 'a':
-			X--;
-			break;
-		case 'd':
-			X++;
-			break;
-		}
-		X = std::clamp(X, 0, 10);
-		Y = std::clamp(Y, 0, 10);
-		COORD Cur;
-		Cur.X = X;
-		Cur.Y = Y;
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-		std::cout << "*";
-
-		flatbuffers::FlatBufferBuilder Builder(1024);
-		auto Data = Move::CreatePositionData(Builder, X, Y);
-		Builder.Finish(Data);
-
-		PacketSize = (int)Builder.GetSize();
-		PacketSize = htonl(PacketSize);
-		int SentBytes = send(ClientSock, (char*)&PacketSize, sizeof(PacketSize), 0);
-		SentBytes = send(ClientSock, (char*)Builder.GetBufferPointer(), (int)Builder.GetSize(), 0);
+		int Result = ProcessPacket(RecvBuffer, ClientSocket);
 	}
 
-	closesocket(ListenSock);
+	closesocket(ListenSocket);
 
 	WSACleanup();
+
+	return 0;
+}
+
+int ProcessPacket(const char* RecvBuffer, SOCKET ClientSocket)
+{
+	//root_type
+	auto RecvEventData = UserEvents::GetEventData(RecvBuffer);
+	std::cout << RecvEventData->timestamp() << std::endl; //타임스탬프
+
+	flatbuffers::FlatBufferBuilder Builder;
+
+	switch (RecvEventData->data_type())
+	{
+	case UserEvents::EventType_C2S_Login:
+	{
+		auto LoginData = RecvEventData->data_as_C2S_Login();
+		if (LoginData->userid() && LoginData->password())
+		{
+			std::cout << "Login Request success: " << LoginData->userid()->c_str() << ", " << LoginData->password()->c_str() << std::endl;
+			CreateS2C_Login(Builder, true, "Login Success", PosX, PosY, UserEvents::Color(255, 255, 255), (uint32_t)ClientSocket);
+		}
+		else
+		{
+			CreateS2C_Login(Builder, false, "empty id, password", 0, 0, UserEvents::Color(0, 0, 0), (uint32_t)ClientSocket);
+		}
+		SendPacket(ClientSocket, Builder);
+	}
+	break;
+	case UserEvents::EventType_C2S_Logout:
+	{
+		auto LogoutData = RecvEventData->data_as_C2S_Logout();
+		if (LogoutData->player_id())
+		{
+			std::cout << "Logout Request success: " << LogoutData->player_id() << std::endl;
+			CreateS2C_Logout(Builder, LogoutData->player_id(), true, "Logout Success");
+		}
+		else
+		{
+			CreateS2C_Logout(Builder, 0, false, "Logout Failed");
+		}
+		SendPacket(ClientSocket, Builder);
+	}
+	break;
+	case UserEvents::EventType_C2S_PlayerMoveData:
+	{
+		auto MoveData = RecvEventData->data_as_C2S_PlayerMoveData();
+		if (MoveData->key_code())
+		{
+			std::cout << "Move Request success: " << MoveData->key_code() << std::endl;
+		}
+		else
+		{
+			std::cout << "Move Request Failed" << std::endl;
+			break;
+		}
+		int MoveX = 0;
+		int MoveY = 0;
+		switch (MoveData->key_code())
+		{
+		case 'w':
+			MoveY--;
+			break;
+		case 's':
+			MoveY++;
+			break;
+		case 'a':
+			MoveX--;
+			break;
+		case 'd':
+			MoveX++;
+			break;
+		default:
+			break;
+		}
+		PosX = std::clamp((int)PosX + MoveX, 0, 10);
+		PosY = std::clamp((int)PosY + MoveY, 0, 10);
+		CreateS2C_PlayerMoveData(Builder, (uint32_t)ClientSocket, PosX, PosY);
+		SendPacket(ClientSocket, Builder);
+	}
+	break;
+	}
 
 	return 0;
 }
